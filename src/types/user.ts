376d3f2a2e1347fc2131f0c62acc44df9deac534
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-01-22 11:11:05 +0530

    Update user.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,22 +1,8 @@
-export type UserRole = 'admin' | 'manager' | 'employee';
-
-export interface User {
-  id: string;
-  email: string;
-  name: string;
-  employee_id: string;
-  designation: string;
-  password: string;
-  created_at?: string;
-  profile_photo?: string;
-  role: UserRole;
-}
-
 export interface UserFormData {
   name: string;
   email: string;
   employeeId: string;
   designation: string;
   password: string;
-  role: UserRole;
+  role: "admin" | "manager" | "employee";
 }
